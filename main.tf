2a0da120cd03554008fdcecb79a003f44aba30a0
Olga Sentsiurova <olga.sentsiurova@gmail.com>
2024-05-31 14:59:51 +0200

    Autoscaling

--- main.tf
+++ main.tf
@@ -33,20 +33,20 @@ module "blog_vpc" {
   }
 }
 
-#module "blog_autoscaling" {
-#  source  = "terraform-aws-modules/autoscaling/aws"
-#  version = "6.5.2"
-#
-#  name = "blog"
-#
-#  min_size            = 1
-#  max_size            = 2
-#  vpc_zone_identifier = module.blog_vpc.public_subnets
-#  target_group_arns   = module.blog_alb.target_group_arns
-#  security_groups     = [module.blog_sg.security_group_id]
-#  instance_type       = var.instance_type
-#  image_id            = data.aws_ami.app_ami.id
-#}
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sq.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
@@ -66,12 +66,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
@@ -88,18 +82,18 @@ module "blog_alb" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sq.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
+#resource "aws_instance" "blog" {
+#  ami           = data.aws_ami.app_ami.id
+#  instance_type = var.instance_type
+#
+#  vpc_security_group_ids = [module.blog_sq.security_group_id]
+#
+#  subnet_id = module.blog_vpc.public_subnets[0]
+#
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
 
 module "blog_sq" {
   source  = "terraform-aws-modules/security-group/aws"
