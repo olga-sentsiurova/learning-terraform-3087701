3cbc0223a9dea1b00700fe7c55c793ca39fa79cb
Olga Sentsiurova <olga.sentsiurova@gmail.com>
2024-05-31 15:47:29 +0200

    fix prefix length

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "${var.environment.name}-blog-"
+      name_prefix      = "dblog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
